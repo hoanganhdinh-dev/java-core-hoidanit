f5b16c14910bd127308b98dd5710a08a9813c54d
Dinh Hoang Anh <hoanganhdinh.dev@gmail.com>
2026-08-14 15:12:19 +0700

    Complete Video 53 - Fields & Methods

---
+++ src/chapter06/video53/Student.java
@@ -0,0 +1,15 @@
+package chapter06.video53;
+
+public class Student {
+	void sleep() {
+		System.out.println("I'm sleeping");
+	}
+
+	void onThi() {
+		System.out.println("I'm studying");
+	}
+
+	int sum(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/chapter06/video53/Video53.java
@@ -0,0 +1,10 @@
+package chapter06.video53;
+
+public class Video53 {
+	public static void main(String[] args) {
+		Student sv1 = new Student();
+		sv1.onThi();
+		int testSum = sv1.sum(6, 9);
+		System.out.println("Test sum = " + testSum);
+	}
+}
