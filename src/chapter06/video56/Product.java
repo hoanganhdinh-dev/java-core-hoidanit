bce95ef77708237f7c8f17753a8f24ad67a072cf
Dinh Hoang Anh <hoanganhdinh.dev@gmail.com>
2026-08-14 16:39:09 +0700

    Complete Video 56 - Constructor Exercise

---
+++ src/chapter06/video56/Product.java
@@ -0,0 +1,15 @@
+package chapter06.video56;
+
+public class Product {
+	String name;
+	double price;
+
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	void display() {
+		System.out.println(this.name + ": " + this.price);
+	}
+}
---
+++ src/chapter06/video56/Video56.java
@@ -0,0 +1,11 @@
+package chapter06.video56;
+
+public class Video56 {
+	public static void main(String[] args) {
+		Product pd1 = new Product("Mobile", 1500.5);
+		Product pd2 = new Product("Laptop", 2500.5);
+
+		pd1.display();
+		pd2.display();
+	}
+}
