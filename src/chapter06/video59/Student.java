9aaa4e23ae2914f5dcb28571073df912be7d7577
Dinh Hoang Anh <hoanganhdinh.dev@gmail.com>
2026-08-15 16:34:11 +0700

    Complete Video 59 - Encapsulation

---
+++ src/chapter06/video59/Student.java
@@ -0,0 +1,21 @@
+package chapter06.video59;
+
+public class Student {
+	private String name;
+	private String grade;
+
+	public Student(String name, String grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+
+	// setter
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// getter
+	public String getName() {
+		return name;
+	}
+}
---
+++ src/chapter06/video59/Video59.java
@@ -0,0 +1,10 @@
+package chapter06.video59;
+
+public class Video59 {
+	public static void main(String[] args) {
+		Student st = new Student("hoidanit", "F");
+		st.setName("update name by setter");
+
+		System.out.println("name = " + st.getName());
+	}
+}
